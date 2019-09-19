61e44dd30fc6ab2ece00d695fd5dbaf2e78dcd1d
bnayae <Bnayanet@hotmail.com>
2019-09-19 11:39:14 +0300

    K8s ready

--- graphql-node-server/server.js
+++ graphql-node-server/server.js
@@ -4,6 +4,7 @@ const schema = require("./schema");
 const cors = require("cors");
 
 const app = express();
+let count = 0;
 app.use(cors());
 app.use(
   "/graphql",
@@ -12,6 +13,15 @@ app.use(
     graphiql: true
   })
 );
+app.get("/health", (req, res) => {
+  res.send(`I'm OK: ${new Date().toISOString()}`);
+});
+app.get("/readiness", (req, res) => {
+  res.send(`I'm ready: ${new Date().toISOString()}`);
+});
+app.get("/metrics", (req, res) => {
+  res.send(`service_demo_connection_health ${count++ % 10 !== 7}`);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
